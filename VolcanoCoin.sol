ae25b0cb167e75b63f6a652d26cd141093fb53d6
Nathan Paik <64183569+nathanpaik@users.noreply.github.com>
2021-11-14 21:12:49 -0800

    Update VolcanoCoin.sol

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 contract VolcanoCoin {
     uint totalSupply;
-    
     address owner;
     
     modifier onlyOwner {
@@ -15,7 +14,34 @@ contract VolcanoCoin {
     constructor() {
         totalSupply = 10000;
         owner = msg.sender;
+        balance[owner] += totalSupply;
+        totalSupply -= 10000;
+        
     }
+    
+    // may be able to use a struct instead of a mapping but will decide later
+    mapping(address => uint) public balance;
+    //struct userBalances {
+        //address userAddress;
+        //uint public view balance;
+    //}
+    
+    event transferIndicator(uint, address);
+    
+    // not sure if this is the preferred way to transfer may need to edit
+    function transer(uint transferAmount, address recipient) public {
+        balance[recipient] += transferAmount;
+        balance[owner] -= transferAmount;
+        emit transferIndicator(transferAmount, recipient);
+    }
+    
+    struct Payment {
+        uint transferAmount;
+        address recipient;
+    }
+    
+    // references an array of payments for each sneder's associated address
+    mapping(address => Payment[]) payments;
     
     function getTotalSupply() public view returns (uint) {
         return totalSupply;
